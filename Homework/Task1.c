7221816649e2a898e2bbc9231c910ffd77cc8cdd
Rosik21 <Rosik2002@ya.ru>
2023-01-03 13:50:06 +0100

    First version

--- Homework/Task1.c
+++ Homework/Task1.c
@@ -1,19 +1,38 @@
 #include<stdio.h>
+#include<string.h>
+
+
 
 int main() {
-	int dArr[10], dOddCtr = 0;
+	char currentFloor[5] = "G";
 
-	for (int i = 0; i < 10; i++) {
-		printf("\n Please enter a [%d] number: ", i);
-		scanf("%d", &dArr[i]);
-	}
-	printf("\n");
-	for (int i = 0; i < 10; i++) {
-		printf("%d ", dArr[i]);
-		if (dArr[i] % 2 != 0) {
-			dOddCtr++;
+	printf("\n You are now on %s floor", currentFloor);
+	printf("\n If you want to exit type \"exit\"");
+	printf("\n Please enter the floor you want to go to: ");
+
+	while (1) {
+
+		scanf("%4s", currentFloor);
+
+		if (strcmp(currentFloor, "exit") == 0) {
+			printf("\n Thank you for using an elevator ");
+			return 0;
+		} else {
+			if (strcmp(currentFloor, "B3") == 0 ||
+				strcmp(currentFloor, "B2") == 0 ||
+				strcmp(currentFloor, "B1") == 0 ||
+				strcmp(currentFloor, "G") == 0 ||
+				strcmp(currentFloor, "1") == 0 ||
+				strcmp(currentFloor, "2") == 0 ||
+				strcmp(currentFloor, "3") == 0 ||
+				strcmp(currentFloor, "4") == 0 ||
+				strcmp(currentFloor, "5") == 0)
+			{
+				printf("\n You are now on %s floor", currentFloor);
+				printf("\n If you want to exit type \"exit\", or select the next floor: ");
+				continue;
+			}
 		}
 	}
-	printf("\n There are %d odd numbers in your array.\n ", dOddCtr);
-	return 0;
 }
+
